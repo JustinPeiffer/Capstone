2b0f9162093c79ff6030e69c2b03f1aa4e11e622
Justin R. Peiffer <46731807+JustinPeiffer@users.noreply.github.com>
2020-11-24 16:50:54 -0600

    Add files via upload
    Modified header to display the login form when not logged in and the logout button when logged in

--- FairMont Community Project/php/header.php
+++ FairMont Community Project/php/header.php
@@ -1,9 +1,122 @@
+<style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+  width:280px;
+}
+
+.topnav .login-container button {
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background-color: #555;
+  color: white;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .login-container button:hover {
+  background-color: green;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .login-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .login-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+</style>
+<div class="topnav">
+<div class="login-container">
 <?php
 	session_start();
-	if (isset($_SESSION['UserID']) {
-		echo <p>You are logged in.</p>
+	if (isset($_SESSION['UID'])) {
+		echo '<form action="http://localhost:8080/FairMont%20Community%20Project/php/logout.php" method="post">
+			<div class="container">
+				<button name="logout-submit" type="submit">Logout</button>
+			</div>
+			</form>';
+	}
+	else {
+			echo '<form action="http://localhost:8080/FairMont%20Community%20Project/php/login.php" method="post">
+			<div class="container">
+				<label for="username"><b>Username or Email</b></label>
+				<input type="text" placeholder="Enter Username or Email" name="username" required>
+				<label for="psw"><b>Password</b></label>
+				<input type="text" placeholder="Enter Password" name="psw" required>
+				<button name="login-submit" type="submit">Login</button>
+				<label><input type="checkbox" checked="checked" name="remember">Remember me</label>
+			</div>
+			<div class="container" style="background-color:#f1f1f1">
+				<span class="psw">Forgot password?<a href="#"></a></span>
+			</div>
+		</form>';
 	}
 ?>
-<!DOCTYPE html>
-<html>
-</html>
+</div>
+	<a href="Main.html"><h1>Fairmount<br>Community</h1></a> 
+</div>
+<!--<img src=".jpg" alt="" width="600" height="600">-->
+
+<?php
+	if (isset($_SESSION['UID'])) {
+		echo '<h2> Hello';
+		echo " ".$_SESSION['fName']." ".$_SESSION['lName'];
+		echo '</h2>';
+	}
+?>
+<nav><ul>
+	<li><a href="Main.html">Home</a></li>
+    <li><a href="Services.html">Services</a></li>
+    <li><a href="Donations.html">Donations</a></li>
+	<li><a href="History.html">History</a></li>
+	<li><a href="SocialWorker.html">Social&nbsp;Worker</a></li>
+	<li><a href="Events.html">Events</a></li>
+	<li><a href="MonthlyMeetings.html">Monthly&nbsp;Meetings</a></li>
+	<li><a href="School.html">School</a></li>
+</ul></nav>
