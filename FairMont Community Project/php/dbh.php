912d1928cde8dae85fe202d7102c3bb3751ab435
Dayne Hultman <daynehultman@gmail.com>
2020-11-24 19:13:09 -0600

    some changes

--- FairMont Community Project/php/dbh.php
+++ FairMont Community Project/php/dbh.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $dBUsername = "root";
 $dBPassword = "usbw" ;
-$dBName = "fairmont community group";
+$dBName = "fcg";
 
 $conn = mysqli_connect($servername, $dBUsername,$dBPassword, $dBName);
 
