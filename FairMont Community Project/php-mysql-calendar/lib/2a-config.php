fc680e9eff71c9755612c3aa50e559d36a2022d9
ProWildDuff <63990551+ProWildDuff@users.noreply.github.com>
2020-11-25 12:41:44 -0600

    Event Calandar and Upload Files Test

--- FairMont Community Project/php-mysql-calendar/lib/2a-config.php
+++ FairMont Community Project/php-mysql-calendar/lib/2a-config.php
@@ -4,10 +4,12 @@ error_reporting(E_ALL & ~E_NOTICE);
  
 // DATABASE SETTINGS - CHANGE THESE TO YOUR OWN
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'test');
+define('DB_NAME', 'events');
 define('DB_CHARSET', 'utf8');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
+
+$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
 // AUTO PATH
 define('PATH_LIB', __DIR__ . DIRECTORY_SEPARATOR);
