2716f7a0504f3bb6cf6515665a1c98184f03641a
ProWildDuff <63990551+ProWildDuff@users.noreply.github.com>
2020-12-10 16:36:06 -0600

    debugged PHPMailer

--- FairMont Community Project/php/phpmailer/Exception.php
+++ FairMont Community Project/php/phpmailer/Exception.php
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 <?php
-
+namespace PHPMailer\PHPMailer;
 /**
  * PHPMailer Exception class.
  * PHP Version 5.5.
@@ -19,9 +18,6 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.
  */
-
-namespace PHPMailer\PHPMailer;
-
 /**
  * PHPMailer exception handler.
  *
@@ -39,45 +35,4 @@ class Exception extends \Exception
         return '<strong>' . htmlspecialchars($this->getMessage()) . "</strong><br />\n";
     }
 }
-=======
-<?php
-
-/**
- * PHPMailer Exception class.
- * PHP Version 5.5.
- *
- * @see       https://github.com/PHPMailer/PHPMailer/ The PHPMailer GitHub project
- *
- * @author    Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
- * @author    Jim Jagielski (jimjag) <jimjag@gmail.com>
- * @author    Andy Prevost (codeworxtech) <codeworxtech@users.sourceforge.net>
- * @author    Brent R. Matzelle (original founder)
- * @copyright 2012 - 2020 Marcus Bointon
- * @copyright 2010 - 2012 Jim Jagielski
- * @copyright 2004 - 2009 Andy Prevost
- * @license   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- * @note      This program is distributed in the hope that it will be useful - WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.
- */
-
-namespace PHPMailer\PHPMailer;
-
-/**
- * PHPMailer exception handler.
- *
- * @author Marcus Bointon <phpmailer@synchromedia.co.uk>
- */
-class Exception extends \Exception
-{
-    /**
-     * Prettify error message output.
-     *
-     * @return string
-     */
-    public function errorMessage()
-    {
-        return '<strong>' . htmlspecialchars($this->getMessage()) . "</strong><br />\n";
-    }
-}
->>>>>>> 5ebc5c5d1bd059025862689e6cd915d2a3bc93ab
+?>
