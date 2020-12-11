93d86022dd29ef90c921af61b9d30a3ea310094b
Justin R. Peiffer <46731807+JustinPeiffer@users.noreply.github.com>
2020-12-11 16:26:12 -0600

    Update header.php

--- FairMont Community Project/php/header.php
+++ FairMont Community Project/php/header.php
@@ -18,30 +18,6 @@
 
 <div class="login-container">
 <?php
-					require_once 'PHPMailer\PHPMailer.php';
-					require_once 'PHPMailer\Exception.php';
-					require_once 'PHPMailer\SMTP.php';
-					use PHPMailer\PHPMailer\PHPMailer;
-					use PHPMailer\PHPMailer\SMTP;
-					use PHPMailer\PHPMailer\Exception;
-					$mail = new PHPMailer();
-					$mail->isSMTP();
-					$mail->Host = 'smtp.gmail.com';
-					$mail->SMTPAuth = true;
-					$mail->SMTPSecure = 'tls';
-					$mail->Port = 587;
-					$mail->Username = 'justinpeiffer@gmail.com';
-					$mail->Password = 'Whovian11';
-					
-					$mail->setFrom('justinpeiffer@gmail.com','Justin Peiffer');
-					$mail->addAddress('justinpeiffer@gmail.com');
-					
-					$mail->Subject = 'My First SMTP email';
-					$mail->Body = 'Fairmont';
-					
-					//$mail->send();
-					$mail->smtpClose();
-					
 	session_start();
 	if (isset($_SESSION['UID'])) {
 		echo '<form action="http://localhost:8080/FairMont%20Community%20Project/php/logout.php" method="post">
